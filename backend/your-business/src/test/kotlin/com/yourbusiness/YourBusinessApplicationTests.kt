2d767337687ce8a7380e64fde96d85f7f1aa6b62
rafasanchezsalado <rafael.sanchez.salado@gmail.com>
2023-06-27 01:44:51 +0200

    feat - backend setup and docker configuration

---
+++ backend/your-business/src/test/kotlin/com/yourbusiness/YourBusinessApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yourbusiness
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class YourBusinessApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
