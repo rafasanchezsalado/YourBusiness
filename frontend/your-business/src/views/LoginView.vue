931e7b76c8d75b0d8cc8432dde62f1174d64f331
rafasanchezsalado <rafael.sanchez.salado@gmail.com>
2023-06-30 19:26:55 +0200

    feat - basic app navigation and defaulting styles

---
+++ frontend/your-business/src/views/LoginView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <main>
+        <router-link to="/home">Login Up</router-link>
+        <br>
+        <router-link to="/setup">Sign Up</router-link>
+    </main>
+</template>
---
+++ frontend/your-business/src/views/SetupView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+    <main>
+        <form action="">
+
+            <h3>Set up your business!</h3>
+            <br>
+            <label>Name</label>
+            <input type="text" value="" />
+            <br>
+            <label>Primary color</label>
+            <input type="color" />
+            <br>
+            <button>Continue</button>
+
+        </form>
+    </main>
+</template>
